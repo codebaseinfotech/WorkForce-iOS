7634a7e291d93c7bff0eb333024890e51b690e4c
Ankit Gabani <prakash@net-informatika.com>
2026-02-25 10:07:38 +0530

    Task working

---
+++ AttendanceSystem/Controller/Tabbar/Task/Cell/TaskListTVCell/View/TaskListTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  TaskListTVCell.swift
+//  AttendanceSystem
+//
+//  Created by Ankit on 25/02/26.
+//
+
+import UIKit
+
+class TaskListTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- AttendanceSystem/Controller/Tabbar/Task/View/TaskVC.swift
+++ AttendanceSystem/Controller/Tabbar/Task/View/TaskVC.swift
@@ -8,7 +8,25 @@
 import UIKit
 
 class TaskVC: UIViewController {
-
+    
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var imgUserProfile: UIImageView!
+    @IBOutlet weak var lblUserName: UILabel!
+    @IBOutlet weak var lblUserProfesion: UILabel!
+    
+    @IBOutlet weak var svInProgress: UIStackView!
+    @IBOutlet weak var svPending: UIStackView!
+    @IBOutlet weak var svCompleted: UIStackView!
+    
+    @IBOutlet weak var tblViewInProgress: UITableView!
+    @IBOutlet weak var tblViewPending: UITableView!
+    @IBOutlet weak var tblVIewCompleted: UITableView!
+    
+    @IBOutlet weak var viewNoDataInProgress: UIView!
+    @IBOutlet weak var viewNoDataPending: UIView!
+    @IBOutlet weak var viewNoDataCompleted: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -36,5 +54,20 @@ class TaskVC: UIViewController {
         navigationController?.pushViewController(vc, animated: false)
     }
     
-
+    // MARK: - Actions
+    @IBAction func tappedChat(_ sender: Any) {
+        let vc = ChatVC()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func tappedNotification(_ sender: Any) {
+        let vc = NotificationVC()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func tappedYearDropDown(_ sender: Any) {
+    }
+    
+    
+    
 }
